18f9a59f8fa731d046bdb5ab1e5812a0751c9128
Vaughan <vaughan@xelerance.com>
2017-01-24 12:31:16 -0500

    add gdialog to prompt for different db params

--- pg3Lanch.sh
+++ pg3Lanch.sh
@@ -1,4 +1,23 @@
-pgadmin3 -qc "host=dhpr-dev2 port=5432 dbname=dhpr-dev2 role=vaughan" -f "$1" &
+#!/bin/bash
+SQL_FILE=$1
+HOST="dhpr-dev2"
+PORT="5432"
+DBNAME="dhpr-dev2"
+ROLE="vaughan"
+
+params=($( \
+  gdialog --title "Run SQL with params" \
+          --inputbox "host port dbname role" 100 600 "$HOST $PORT $DBNAME $ROLE" \
+   2>&1
+ ))
+
+HOST=${params[0]}
+PORT=${params[1]}
+DBNAME=${params[2]}
+ROLE=${params[3]}
+
+echo "connecting to host=$HOST port=$PORT dbname=$DBNAME role=$ROLE"
+pgadmin3 -qc "host=$HOST port=$PORT dbname=$DBNAME role=$ROLE" -f "$SQL_FILE" &
 sleep .6
 window=$(xdotool search --class pgadmin3 |tail -n 1)
 xdotool key --window $window F5 #send F5 keystroke to window to execute the query
